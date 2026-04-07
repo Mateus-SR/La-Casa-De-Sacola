84d95a46ee66f2af219d7de9e780a7e5ec2adacf
Rafaela <rafaelaolbo@gmail.com>
2026-04-06 23:49:50 -0300

    Adicionando home, prototipo

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,5 @@
-import { redirect } from "next/navigation";
-
-export default function Home() {
-  redirect("/login");
-}
+export { default } from "./home";
 
 /* Beca- Não Apaguem isso é IMPORTANTE!! */
+/* Rota original de redirect para login mantida em: src/app/login/page.js */
+/* Para voltar ao redirect: import { redirect } from 'next/navigation'; export default function Home() { redirect('/login'); } */
